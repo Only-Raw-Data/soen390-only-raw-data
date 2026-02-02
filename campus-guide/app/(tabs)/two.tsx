dc41d5dedfa94d5d6bbf0951ed7818b9d1b87699
TristanGirardi <t.girardi13@gmail.com>
2026-02-02 15:49:39 -0500

    Added map to directions tab

--- campus-guide/app/(tabs)/two.tsx
+++ campus-guide/app/(tabs)/two.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from '../components/Header';
 import { DirectionsHeader } from '../components/DirectionsHeader';
+import { MapViewApp } from '../components/MapView';
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
       <Header />
       <DirectionsHeader />
+      <MapViewApp showSearch={false} googleMapsApiKey={process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}/>
     </View>
   );
 }
